0f43bd2d8ec9a0208d59a3612b778cda3da0d4d8
tchawla827 <155444812+tchawla827@users.noreply.github.com>
2025-07-02 16:35:30 +0530

    Refactor routing and wrap with CartProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Placeholder from './pages/Placeholder';
 import ProductSearch from './pages/ProductSearch';
 import CartPage from './pages/CartPage';
 import { CartProvider } from './context/CartContext';
 
 const App: React.FC = () => {
   return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<ProductSearch />} />
-          <Route path="/products" element={<ProductSearch />} />
-          <Route path="/pantry/setup" element={<Placeholder title="SmartPantry Setup" />} />
-          <Route path="/gift" element={<Placeholder title="GiftGenius" />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/profile" element={<Placeholder title="Profile" />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <div className="container mx-auto p-4">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<ProductSearch />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </BrowserRouter>
   );
 };
 
